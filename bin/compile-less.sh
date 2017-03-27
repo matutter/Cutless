84475b47e2cc510be4a0953d829c778fc3c916de
Mat Utter <matutter4@gmail.com>
2017-03-27 07:50:12 -0400

    compile-less: added check for lessc

--- bin/compile-less.sh
+++ bin/compile-less.sh
@@ -12,12 +12,21 @@ CSS=$SOURCE/static/css
 
 COMPILER=$(which lessc)
 
+if [ -z "$COMPILER" ]; then
+  echo "Cannot find less transpiler (lessc)." 1>&2
+  exit
+fi
+
 while read IN_FILE; do
 
   OUT_FILE=${IN_FILE/$LESS/$CSS}
   OUT_FILE=${OUT_FILE/'.less'/'.css'}
-  echo "compiling $(basename ${IN_FILE})"
 
+  if [ -f $OUT_FILE ]; then
+    rm -f $OUT_FILE
+  fi
+
+  echo "$(basename ${IN_FILE}) > $(basename ${OUT_FILE})"
   $COMPILER $IN_FILE $OUT_FILE
 
 done<<<"$(find $LESS -type f -name '*.less' | grep -v mixin)"

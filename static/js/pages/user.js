992254d807b27fe86d369425825d9adee38ba276
matutter <matutter4@gmail.com>
2017-06-12 01:26:48 +0000

    added bootstrapv4-alpha, tether, and package-lock

--- static/js/pages/user.js
+++
@@ -1,9 +0,0 @@
-// js for pages /users/*
-
-function loadHomePage() {
-  window.location = '/'
-}
-
-// how to response to api-success responses
-$.dropclient.on('/users/login', loadHomePage)
-$.dropclient.on('/users/register', loadHomePage)
--- static/js/pages/users/settings.js
+++
@@ -1,9 +0,0 @@
-/**
-* local script for /users/settings
-*/
-
-if(!window.dropclient) throw Error('This module must be loaded after dropclient.js!')
-
-$.dropclient.run(function() {
-
-});

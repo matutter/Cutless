a1847e461b3e048dcc456d284bb08e355e768dc0
matutter <matutter4@gmail.com>
2017-06-18 03:50:36 +0000

    users: added settings page

--- public/index.js
+++ public/index.js
@@ -1,6 +1,17 @@
 
-$(document).click(function() {
+$(document).ready(function() {
   
-  console.log('index - loaded')
+  //console.log('index - loaded');
+  var page_links = $('#page-nav a');
+  if(page_links.length > 0) {
+    var pathname = window.location.pathname.split(/[#?]/)[0];
+    var active = page_links.toArray().map($).find(function(el) {
+      return el.attr('href') == pathname;
+    });
+    if(active.length == 1) {
+      page_links.removeClass('active');
+      active.addClass('active');
+    }
+  }
   
 });
--- public/users/scripts/logout.js
+++ public/users/scripts/logout.js
@@ -1,9 +1,21 @@
 
-$.ajax({
-  url : '/api/users/logout',
-  method: 'post'
-}).done(function() {
-  
+function goHome() {
   window.location.href = '/';
+}
+
+setTimeout(function() {
+
+  console.log("Logging out user...");
+  $.ajax({
+    url : '/users/logout/json',
+    method: 'post'
+  })
+  .done(goHome)
+  .fail(function(e) {
+    $('#log-out-message').text(e.responseText);
+    $('.fa-spin').removeClass('fa-spin');
+    setTimeout(goHome, 2000);
+  });
   
-});
+}, 1000)
+
